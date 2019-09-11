72691211c865ed76954b187f45ac5aca2261aa2a
Stein <steingrimurkj@gmail.com>
2019-09-12 00:39:22 +0200

    fix

--- src/main/java/rest/ResourceStudent.java
+++ src/main/java/rest/ResourceStudent.java
@@ -18,7 +18,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 //Todo Remove or change relevant parts before ACTUAL use
-@Path("student")
+@Path("groupmembers")
 public class ResourceStudent {
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
                 "pu",
